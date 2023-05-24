9c7edb1ad3e4aaa8390a3259923fa87c7f34a029
Beiker Guillen <beiker19998@gmail.com>
2023-05-23 21:43:04 -0400

    operadores mat

---
+++ 01_basico/05_operadores_mat.dart
@@ -0,0 +1,41 @@
+/**
+ * 
+ *  Un operador es un simbolo que le dice al compilador
+ * que debe realizar una tarea
+ * matematica, relacional o logiaca y debe 
+ * de producir un resultado
+ * 
+ * 
+ */
+
+
+main(){
+
+  int a = 10 + 5; // + 15
+
+  a = 20 - 10;    // - 10
+
+  a = 10 * 2      // * 20
+
+  double  b = 10 / 2; //   /  5
+
+  b = 10.0 % 3;       //   %  1  El sobrante de la division
+  b = -b;             // -expresion - Es usado para cambiar el valor de la expresion 
+
+
+  int c = 10 ~/ 3;    // ~/ 3  La division comun y corriente pero solo la parte entera
+
+
+  int d = 1;
+
+  d++;                //    ++    2
+  d--;                //    --    1
+
+  d += 2;             //    +=    3
+  d -= 2;             //    -=    3       *=    /=
+
+  
+
+
+
+}
