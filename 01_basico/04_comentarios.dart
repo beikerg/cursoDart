28f998a60cc5492aeae4159dc8b7330a8251feb0
Beiker Guillen <beiker19998@gmail.com>
2023-05-23 21:34:35 -0400

    Ejercicio 04 comentarios y buenas practivas de documentacion

---
+++ 01_basico/04_comentarios.dart
@@ -0,0 +1,38 @@
+/**
+ * 
+ *  Estos son para documentación es la manera correcta de hacerlo
+ * 
+ * Este archivo sirve para explicar los comentarios
+ * 
+ * 
+ * [creadoEn] : 23-05-2023 
+ * 
+ */
+
+main() {
+  // Esta es una variable temporal que quiere definir
+  int a = 10; // A debería ser 10
+
+  /*
+  final personas = [
+    // Esta es la primera persona
+    'Fernando', // [0]
+
+    // Esta es la segunda persona
+    'Pedro', // [1]
+
+    // ESta es la tercera persona
+    'Carlos' // [2]
+  ];
+
+  print(personas);
+
+  */
+}
+
+// Nombre: nombre de la persona que quiero saludar
+// Mensaje: Es el saludo que quiero hacer
+
+/// Funciona para saludar
+/// Recibe un [nombre] y se concatena con el [mensaje]
+saludar(int nombre, int mensaje) {}
